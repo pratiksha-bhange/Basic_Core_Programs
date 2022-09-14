6004490fa678da60548da25197fb699288ef774d
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 00:53:40 +0530

    [Pratu]ADDED: Compute Quotient And Remainder

---
+++ QuotientAndRemainder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace QuotientAndRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividend, divisor, quotient, remainder;
+
+            Console.WriteLine("Enter Dividend and divisor");
+            dividend = Convert.ToInt32(Console.ReadLine());
+            divisor = Convert.ToInt32(Console.ReadLine());
+
+            quotient = dividend / divisor;
+            remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient is " + quotient);
+            Console.WriteLine("Remainder is " + remainder);
+        }
+    }
+    
+}
