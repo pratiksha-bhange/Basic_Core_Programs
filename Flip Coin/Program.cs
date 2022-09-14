53838c7f505d71475a49e01c31cda74eedb0d052
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-14 22:47:36 +0530

    [Pratu]ADDED: Method For Flip Coin Problem

---
+++ Flip Coin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Flip_Coin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the number of times coin flip");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int heads = 0;
+            int tails = 0;
+            int temp = number;
+            while (number > 0)
+            {
+                if (random.NextDouble() <= 0.5)
+                    heads++;
+                else
+                    tails++;
+                number--;
+
+            }
+            number = temp;
+            double percentageOfHead = heads * 100.0 / number;
+            double percentageOfTail = tails * 100.0 / number;
+            Console.WriteLine("Percentage of Head is " + percentageOfHead);
+            Console.WriteLine("Percentage of tail is " + percentageOfTail);
+        }
+    }
+}
