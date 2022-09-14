8b81a8f6fa136cd3f0e3a7be4b7cd32c654c0b2e
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 00:44:03 +0530

    [Pratu]ADDED: Method Of Check leap Year

---
+++ LeapYear/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the year in four digits :- ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0 || year % 400 == 0)
+            {
+                Console.WriteLine("year is leap year");
+            }
+            else
+            {
+                Console.WriteLine("year is not leap year");
+            }
+            Console.ReadLine();
+        }
+    }
+}
