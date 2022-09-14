8966f9914bbae0698ba97a5ad52b79047f8bcce1
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 00:54:34 +0530

    [Pratu]ADDED: Method Of Swap Two Numbers

---
+++ SwapTwoNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+            Console.Write("Enter the First Number : ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Second Number : ");
+            num2 = int.Parse(Console.ReadLine());
+
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.Write("After Swapping : ");
+            Console.Write("\nFirst Number : " + num1);
+            Console.Write("\nSecond Number : " + num2);
+
+            Console.Read();
+        }
+    }
+}
