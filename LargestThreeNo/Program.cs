2eb5dcdaad375e7a094ffd88d41d7eee09b051b4
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 01:00:28 +0530

    [Pratu]ADDED: Largest Among Three Numbers

---
+++ LargestThreeNo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LargestThreeNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first;
+            int second;
+            int third;
+
+            Console.WriteLine("Enter the first number : ");
+            first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number : ");
+            second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number : ");
+            third = int.Parse(Console.ReadLine());
+
+            int maxValue = Math.Max(Math.Max(first, second), third);
+
+            Console.WriteLine("Largest value : " + maxValue);
+        }
+    }
+}
