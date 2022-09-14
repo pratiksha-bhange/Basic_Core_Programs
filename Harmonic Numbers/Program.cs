4d39ba564e0d0009657074d78eb1addab13c8a8b
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 00:49:41 +0530

    [Pratu]ADDED: Method Of Nth Harmonic No

---
+++ Harmonic Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Harmonic_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of N: ");
+
+            //VARIABLES
+            int N = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            float nthHarmonicValue = 0;
+
+            while (i <= N)
+            {
+                Console.Write($"1/{i} + ");
+                nthHarmonicValue += 1 / (float)i;
+                i++;
+            }
+            Console.WriteLine($"\n{nthHarmonicValue}");
+        }
+    }
+}
