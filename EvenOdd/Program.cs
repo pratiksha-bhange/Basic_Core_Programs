4d1fce5ec4065be74b2f5937dfc7ca6489b5e586
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 00:55:56 +0530

    [Pratu]ADDED: Check weather It Is  Even Or Odd

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any number");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            if (Num % 2 == 0)
+            {
+                Console.WriteLine("Enter number is even");
+            }
+            else
+            {
+                Console.WriteLine("Enter number is odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
