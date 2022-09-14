c1726cfbcc39a446522087df8604e16b4857f62b
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 00:57:28 +0530

    [Pratu]ADDED: Check Which Is Vowel Or Consonant

---
+++ VowelOrConsonant/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VowelOrConsonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a character to check wheather it is Vowel or Consonant : ");
+            char character = Convert.ToChar(Console.ReadLine().ToUpper());
+            switch (character)
+            {
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    Console.WriteLine("Entered character is a Vowel");
+                    break;
+                default:
+                    Console.WriteLine("Entered character is a Consonant");
+                    break;
+            }
+        }
+    }
+}
